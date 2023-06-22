71f4a4bb0b8f31b3d79eb501f1e2958e8208ba12
blink <zaneproduction18@gmail.com>
2023-06-22 18:07:52 -0400

    For remove_prefix.sh: - Added changes to redundant methods - Added safe checks for functions

--- remove_prefix.sh
+++ remove_prefix.sh
@@ -12,28 +12,28 @@ spacer()
 set +x
 function red(){
     echo -e "\x1B[31m $1 \x1B[0m"
-    if [ ! -z "${2}" ]; then
+    if [ -n "${2}" ]; then
     echo -e "\x1B[31m $($2) \x1B[0m"
     fi
 }
 
 function green(){
     echo -e "\x1B[32m $1 \x1B[0m"
-    if [ ! -z "${2}" ]; then
+    if [ -n "${2}" ]; then
     echo -e "\x1B[32m $($2) \x1B[0m"
     fi
 }
 
 function yellow(){
     echo -e "\x1B[33m $1 \x1B[0m"
-    if [ ! -z "${2}" ]; then
+    if [ -n "${2}" ]; then
     echo -e "\x1B[33m $($2) \x1B[0m"
     fi
 }
 
 function cyan(){
     echo -e "\x1B[36m $1 \x1B[0m"
-    if [ ! -z "${2}" ]; then
+    if [ -n "${2}" ]; then
     echo -e "\x1B[36m $($2) \x1B[0m"
     fi
 }
@@ -42,7 +42,7 @@ replace()
 {
   dir_check="n"
 
-  cd "$directory"
+  cd "$directory" || exit 1
   current_dir="${PWD}/"  
   
   spacer; cyan "Current file path: $current_dir"
