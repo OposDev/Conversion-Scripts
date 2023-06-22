c009ba860d43fe914c2a6fdf89b35be11ee26a59
blink <zaneproduction18@gmail.com>
2023-06-22 18:07:30 -0400

    For reddit_archive.sh: - Organized methods in C style

--- reddit_archive.sh
+++ reddit_archive.sh
@@ -10,28 +10,28 @@ links_total=0
 set +x
 function red(){
     echo -e "\x1B[31m $1 \x1B[0m"
-    if [ ! -z "${2}" ]; then
+    if [ -n "${2}" ]; then
     echo -e "\x1B[31m $($2) \x1B[0m"
     fi
 }
 
 function green(){
     echo -e "\x1B[32m $1 \x1B[0m"
-    if [ ! -z "${2}" ]; then
+    if [ -n "${2}" ]; then
     echo -e "\x1B[32m $($2) \x1B[0m"
     fi
 }
 
 function yellow(){
     echo -e "\x1B[33m $1 \x1B[0m"
-    if [ ! -z "${2}" ]; then
+    if [ -n "${2}" ]; then
     echo -e "\x1B[33m $($2) \x1B[0m"
     fi
 }
 
 function cyan(){
     echo -e "\x1B[36m $1 \x1B[0m"
-    if [ ! -z "${2}" ]; then
+    if [ -n "${2}" ]; then
     echo -e "\x1B[36m $($2) \x1B[0m"
     fi
 }
